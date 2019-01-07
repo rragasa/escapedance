485e0fceb6ab64b25cfaba793590ffd171b58f3d
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-01-07 14:48:14 +0000

    [issue#6] - set up custom header feature

--- functions.php
+++ functions.php
@@ -1,8 +1,31 @@
+
 <?php
-// Register WP Menu
-function register_my_menu()
-{
+/**
+ * Escape Dance functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage escapedance
+ * @since 1.0
+ */
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function escapedance_setup() {
+    // Register navigation menu
     register_nav_menu('primary', __('Primary menu'));
 }
-add_action('init', 'register_my_menu');
+
+add_action( 'after_setup_theme', 'escapedance_setup' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_parent_theme_file_path( '/inc/custom-header.php' );
 ?>
---
+++ inc/custom-header.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Custom header implementation
+ *
+ * @link https://codex.wordpress.org/Custom_Headers
+ *
+ * @package WordPress
+ * @subpackage escapedance
+ * @since 1.0
+ */
+
+ /**
+ * Set up the WordPress core custom header feature.
+ *
+ * @uses escapedance_header_style()
+ */
+function escapedance_custom_header_setup() {
+
+	/**
+	 * Filter Escape Dance custom-header support arguments.
+	 *
+	 * @since Escape Dance 1.0
+	 *
+	 * @param array $args {
+	 *     An array of custom-header support arguments.
+	 *
+	 *     @type string $default-image     		Default image of the header.
+	 *     @type string $default_text_color     Default color of the header text.
+	 *     @type int    $width                  Width in pixels of the custom header image. Default 954.
+	 *     @type int    $height                 Height in pixels of the custom header image. Default 1300.
+	 *     @type string $wp-head-callback       Callback function used to styles the header image and text
+	 *                                          displayed on the blog.
+	 *     @type string $flex-height     		Flex support for height of header.
+	 * }
+	 */
+	add_theme_support( 'custom-header', apply_filters( 'escapedance_custom_header_args', array(
+		'default-image'      => get_parent_theme_file_uri( '/assets/images/Escape_Dance_Hol_31.jpg' ),
+		'width'              => 2000,
+		'height'             => 1200,
+		'flex-height'        => true,
+		'video'              => true,
+		'wp-head-callback'   => 'escapedance_header_style',
+	) ) );
+
+	register_default_headers( array(
+		'default-image' => array(
+			'url'           => '%s/assets/images/Escape_Dance_Hol_31.jpg',
+			'thumbnail_url' => '%s/assets/images/Escape_Dance_Hol_31.jpg',
+			'description'   => __( 'Default Header Image', 'escapedance' ),
+		),
+	) );
+}
+add_action( 'after_setup_theme', 'escapedance_custom_header_setup' );
+?>
