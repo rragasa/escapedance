cf81182a1daf3ba7f1848d640458b191597f3573
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-01-12 17:46:28 +0000

    [issue#12] - adding index and content

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -9,28 +9,118 @@
  * @since 1.0
  */
 
-if ( ! function_exists( 'escapedance_edit_link' ) ) :
-    /**
-     * Returns an accessibility-friendly link to edit a post or page.
-     *
-     * This also gives us a little context about what exactly we're editing
-     * (post or page?) so that users understand a bit more where they are in terms
-     * of the template hierarchy and their content. Helpful when/if the single-page
-     * layout with multiple posts/pages shown gets confusing.
-     */
-    function escapedance_edit_link() {
-        edit_post_link(
-            sprintf(
-                /* translators: %s: Name of current post */
-                __( 'Edit<span class="screen-reader-text"> "%s"</span>', 'escapedance' ),
-                get_the_title()
-            ),
-            '<span class="edit-link">',
-            '</span>'
-        );
-    }
-    endif;
+if ( ! function_exists( 'escapedance_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ */
+function escapedance_posted_on() {
 
+	// Get the author name; wrap it in a link.
+	$byline = sprintf(
+		/* translators: %s: post author */
+		__( 'by %s', 'escapedance' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author() . '</a></span>'
+	);
+
+	// Finally, let's write all of this to the page.
+	echo '<span class="posted-on">' . escapedance_time_link() . '</span><span class="byline"> ' . $byline . '</span>';
+}
+endif;
+
+
+if ( ! function_exists( 'escapedance_time_link' ) ) :
+/**
+ * Gets a nicely formatted string for the published date.
+ */
+function escapedance_time_link() {
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		get_the_date( DATE_W3C ),
+		get_the_date(),
+		get_the_modified_date( DATE_W3C ),
+		get_the_modified_date()
+	);
+
+	// Wrap the time string in a link, and preface it with 'Posted on'.
+	return sprintf(
+		/* translators: %s: post date */
+		__( '<span class="screen-reader-text">Posted on</span> %s', 'escapedance' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+}
+endif;
+
+
+if ( ! function_exists( 'escapedance_entry_footer' ) ) :
+/**
+ * Prints HTML with meta information for the categories, tags and comments.
+ */
+function escapedance_entry_footer() {
+
+	/* translators: used between list items, there is a space after the comma */
+	$separate_meta = __( ', ', 'escapedance' );
+
+	// Get Categories for posts.
+	$categories_list = get_the_category_list( $separate_meta );
+
+	// Get Tags for posts.
+	$tags_list = get_the_tag_list( '', $separate_meta );
+
+	// We don't want to output .entry-footer if it will be empty, so make sure its not.
+	if ( ( ( escapedance_categorized_blog() && $categories_list ) || $tags_list ) || get_edit_post_link() ) {
+
+		echo '<footer class="entry-footer">';
+
+			if ( 'post' === get_post_type() ) {
+				if ( ( $categories_list && escapedance_categorized_blog() ) || $tags_list ) {
+					echo '<span class="cat-tags-links">';
+
+						// Make sure there's more than one category before displaying.
+						if ( $categories_list && escapedance_categorized_blog() ) {
+							echo '<span class="cat-links">' . escapedance_get_svg( array( 'icon' => 'folder-open' ) ) . '<span class="screen-reader-text">' . __( 'Categories', 'escapedance' ) . '</span>' . $categories_list . '</span>';
+						}
+
+						if ( $tags_list && ! is_wp_error( $tags_list ) ) {
+							echo '<span class="tags-links">' . escapedance_get_svg( array( 'icon' => 'hashtag' ) ) . '<span class="screen-reader-text">' . __( 'Tags', 'escapedance' ) . '</span>' . $tags_list . '</span>';
+						}
+
+					echo '</span>';
+				}
+			}
+
+			escapedance_edit_link();
+
+		echo '</footer> <!-- .entry-footer -->';
+	}
+}
+endif;
+
+
+if ( ! function_exists( 'escapedance_edit_link' ) ) :
+/**
+ * Returns an accessibility-friendly link to edit a post or page.
+ *
+ * This also gives us a little context about what exactly we're editing
+ * (post or page?) so that users understand a bit more where they are in terms
+ * of the template hierarchy and their content. Helpful when/if the single-page
+ * layout with multiple posts/pages shown gets confusing.
+ */
+function escapedance_edit_link() {
+	edit_post_link(
+		sprintf(
+			/* translators: %s: Name of current post */
+			__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'escapedance' ),
+			get_the_title()
+		),
+		'<span class="edit-link">',
+		'</span>'
+	);
+}
+endif;
 
 /**
  * Display a front page section.
@@ -57,6 +147,51 @@ function escapedance_front_page_section( $partial = null, $id = 0 ) {
 		wp_reset_postdata();
 	} elseif ( is_customize_preview() ) {
 		// The output placeholder anchor.
-		echo '<article class="panel-placeholder panel twentyseventeen-panel twentyseventeen-panel' . $id . '" id="panel' . $id . '"><span class="twentyseventeen-panel-title">' . sprintf( __( 'Front Page Section %1$s Placeholder', 'twentyseventeen' ), $id ) . '</span></article>';
+		echo '<article class="panel-placeholder panel escapedance-panel escapedance-panel' . $id . '" id="panel' . $id . '"><span class="escapedance-panel-title">' . sprintf( __( 'Front Page Section %1$s Placeholder', 'escapedance' ), $id ) . '</span></article>';
 	}
 }
+
+/**
+ * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
+ */
+function escapedance_categorized_blog() {
+	$category_count = get_transient( 'escapedance_categories' );
+
+	if ( false === $category_count ) {
+		// Create an array of all the categories that are attached to posts.
+		$categories = get_categories( array(
+			'fields'     => 'ids',
+			'hide_empty' => 1,
+			// We only need to know if there is more than one category.
+			'number'     => 2,
+		) );
+
+		// Count the number of categories that are attached to the posts.
+		$category_count = count( $categories );
+
+		set_transient( 'escapedance_categories', $category_count );
+	}
+
+	// Allow viewing case of 0 or 1 categories in post preview.
+	if ( is_preview() ) {
+		return true;
+	}
+
+	return $category_count > 1;
+}
+
+
+/**
+ * Flush out the transients used in escapedance_categorized_blog.
+ */
+function escapedance_category_transient_flusher() {
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+	// Like, beat it. Dig?
+	delete_transient( 'escapedance_categories' );
+}
+add_action( 'edit_category', 'escapedance_category_transient_flusher' );
+add_action( 'save_post',     'escapedance_category_transient_flusher' );
--- index.php
+++ index.php
@@ -10,125 +10,58 @@
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage escapedance
+ * @subpackage Twenty_Seventeen
  * @since 1.0
  * @version 1.0
  */
 
 get_header(); ?>
 
+<div class="wrap">
+	<?php if ( is_home() && ! is_front_page() ) : ?>
+		<header class="page-header">
+			<h1 class="page-title"><?php single_post_title(); ?></h1>
+		</header>
+	<?php else : ?>
+	<header class="page-header">
+		<h2 class="page-title"><?php _e( 'Posts', 'escapedance' ); ?></h2>
+	</header>
+	<?php endif; ?>
 
-<!-- Section 1-->
-<section class="content bg-green">
-    <div class="clearfix">
-        <div class="escape-icon col sm-col-3 md-col-3">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/ESCAPE-Social-Media-Icon.png'; ?>" alt="Escape Social Media Icon">
-        </div>
-        <div class="col col-12 sm-col-9 md-col-9">
-            <h2 class="heading pb1">A DANCE FOCUSED HOLIDAY</h2>
-            <h3 class="subheading pb2">Are you ready to switch from work and embark on the experience of a lifetime?</h3>
-            <p class="bold pb1">Full ESCAPE Package: <span class="regular">Thursday 16th May - Thursday 23rd May</span></p>
-            <p class="bold">Weekend ESCAPE Package <span class="regular">Thursday 16th May - Sunday 19th May</span></p>
-        </div>
-    </div>
-</section>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-<!-- Section 2-->
-<section class="content bg-white">
-        <p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-        literature
-        from
-        45
-        BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked
-        up
-        one
-        of
-        the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-        classical
-        literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-        Bonorum
-        et
-        Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
-        very
-        popular
-        during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32.</p>
-</section>
+			<?php
+			if ( have_posts() ) :
 
-<!-- Section 3 -->
-<section class="content bg-green">
-    <p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-        literature
-        from
-        45
-        BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked
-        up
-        one
-        of
-        the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
-        classical
-        literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-        Bonorum
-        et
-        Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
-        very
-        popular
-        during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32.</p>
-    <div class="right-align">
-        <button class="btn btn--orange">Book Now</button>
-    </div>
-</section>
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-<div id="testimonials" class="parallax-container"
-     data-parallax="scroll"
-     data-bleed="10"
-     data-speed="0.2"
-     data-image-src="<?php echo get_template_directory_uri() . '/assets/images/Escape_Dance_Hol_408.jpg'; ?>">
-    <div class="content">
-        <div class="clearfix">
-            <blockquote class="h3 green col col-12 sm-col-6 md-col-5">Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been
-                the
-                industry's
-                standard
-                dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
-                book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
-                more
-                recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. <span class="bold">-
-                    Jackie</span>
-            </blockquote>
-        </div>
-        <button class="btn btn--orange">Book Now</button>
-    </div>
-</div>
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/post/content', get_post_format() );
 
-<!-- Section 4 -->
-<section id="watch" class="content bg-green">
-    <h3 class="h3 center pb3">Watch</h3>
-    <div class="videoWrapper center m3">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ml_V8Ikr3dU?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen></iframe>
-    </div>
-</section>
+				endwhile;
 
-<!-- Section 5-->
-<section id="follow" class="content bg-white">
-    <h3 class="h3 center green pb3">Follow</h3>
-    <ul class="flex-container row">
-        <li class="flex-item"><img src="<?php echo get_template_directory_uri() . '/assets/images/Escape_Dance_Hol_572.jpg'; ?>"></li>
-        <li class="flex-item"><img src="<?php echo get_template_directory_uri() . '/assets/images/Escape_Dance_Hol_574.jpg'; ?>"></li>
-        <li class="flex-item"><img src="<?php echo get_template_directory_uri() . '/assets/images/Escape_Dance_Hol_564.jpg'; ?>"></li>
-        <li class="flex-item"><img src="<?php echo get_template_directory_uri() . '/assets/images/Escape_Dance_Hol_408.jpg'; ?>"></li>
-        <li class="flex-item"><img src="<?php echo get_template_directory_uri() . '/assets/images/Escape_Dance_Hol_517.jpg'; ?>"></li>
-        <li class="flex-item"><img src="<?php echo get_template_directory_uri() . '/assets/images/Escape_Dance_Hol_466.jpg'; ?>"></li>
+				the_posts_pagination( array(
+					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+				) );
 
-    </ul>
-    <ul class="social-icons icon-circle center p3">
-        <li class="inline-block p1"> <a href="https://www.instagram.com/escapedanceholiday/"><i class="fa fa-instagram"></i></a></li>
-        <li class="inline-block p1"> <a href="https://www.facebook.com/escapedanceholiday"><i class="fa fa-facebook"></i></a></li>
-    </ul>
-</section>
+			else :
 
-<?php get_footer(); ?>
+				get_template_part( 'template-parts/post/content', 'none' );
+
+			endif;
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
+
+<?php get_footer();
