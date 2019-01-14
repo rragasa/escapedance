572cef0af6004183d46a86941777eb8d5d6b1010
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2019-01-14 09:32:59 +0000

    [issue-13] :rocket: increasing front page sections

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -82,7 +82,7 @@ function escapedanceholiday_panel_count() {
 	 *
 	 * @param int $num_sections Number of front page sections.
 	 */
-	$num_sections = apply_filters( 'escapedanceholiday_front_page_sections', 4 );
+	$num_sections = apply_filters( 'escapedanceholiday_front_page_sections', 8 );
 
 	// Create a setting and control for each of the sections available in the theme.
 	for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -26,13 +26,6 @@
 
 	<div class="panel-content">
 		<div class="wrap">
-			<header class="entry-header">
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-
-				<?php escapedanceholiday_edit_link( get_the_ID() ); ?>
-
-			</header><!-- .entry-header -->
-
 			<div class="entry-content">
 				<?php
 					/* translators: %s: Name of current post */
